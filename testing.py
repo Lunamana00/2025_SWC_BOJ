f21cfd933d3eb5ad33478af559e9c23d7592fec3
김태현 <142918648+Lunamana00@users.noreply.github.com>
2025-03-15 02:32:41 +0900

    [FEAT] Data process

--- testing.py
+++ testing.py
@@ -4,37 +4,84 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
 
-# Chrome 드라이버 경로 설정 (chromedriver가 PATH에 있다면 생략 가능)
+# Chrome 드라이버 경로 설정
 chrome_driver_path = './chromedriver_win64/chromedriver.exe'  
-# 혹은 chromedriver의 절대경로
+db_path = './DB'
+db_path_keys = db_path+'/keys.txt'
+db_path_cp = db_path+'/checkpoint.txt'
+with open(db_path_keys, "r", encoding="utf-8") as file:
+    content = file.read()
 
-# Chrome 옵션 설정 (헤드리스 모드 사용 예)
+    id = content.split("\n")[0]
+    pw = content.split("\n")[1]
+
+with open(db_path_cp, "r", encoding="utf-8") as file:
+    content = file.read()
+
+    cp = content
+    print(len(cp))
+    print(cp)
+
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # 브라우저 창을 띄우지 않음
+#chrome_options.add_argument("--headless")  # 브라우저 창을 띄우지 않음
 
 # 드라이버 서비스 생성 및 브라우저 실행
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-try:
-    # 목표 URL로 이동
-    url = "https://www.acmicpc.net/status?group_id=23106"
-    driver.get(url)
 
-    # 페이지가 완전히 로드될 때까지 잠시 대기
+
+try:
+    
+    # 로그인인
+    boj_login = "https://www.acmicpc.net/login?next=%2F"
+    driver.get(boj_login)
+
     time.sleep(3)
-    print(11)
-    """
-    # 채점 현황 테이블 찾기 (페이지 구조에 따라 selector 수정 필요)
-    # 일반적으로 테이블은 <table> 태그 내에 있으며, tbody > tr 형태로 각 행이 있음
+    
+    id_input = driver.find_element(By.CSS_SELECTOR, "#login_form > div:nth-child(2) > input")
+    id_input.send_keys(id)
+
+    pw_input = driver.find_element(By.CSS_SELECTOR, "#login_form > div:nth-child(3) > input")
+    pw_input.send_keys(pw)
+
+    login_button = driver.find_element(By.CSS_SELECTOR, "#submit_button")
+    login_button.click()
+
+    time.sleep(3)
+    
+    driver.get("https://www.acmicpc.net/status?group_id=23106")
+
+    time.sleep(2)
+
+    catch_flag = 1
+    first_elem = 0
+
     table_rows = driver.find_elements(By.CSS_SELECTOR, "table.table tbody tr")
 
+    class boj_data:
+        def __init__(self,num,id,problem,tier):
+            self.num = num
+            self.id = id
+            self.problem = problem
+            self.tier  = tier
+
     # 각 행의 데이터를 추출
+    save_data = list()
+
     for row in table_rows:
-        cells = row.find_elements(By.TAG_NAME, "td")
-        # 각 셀의 텍스트를 리스트로 저장
-        data = [cell.text for cell in cells]
-        print(data)
-    """
+        if first_elem == cp:
+            break
+        if row.find_element(By.CLASS_NAME, 'result').text=='맞았습니다!!':
+            boj_num = row.find_elements(By.TAG_NAME, 'td')[0].text
+            boj_id = row.find_elements(By.TAG_NAME, 'td')[1].text
+            boj_problem = row.find_elements(By.TAG_NAME, 'td')[2].text[1:]
+            boj_tier_elem = row.find_element(By.CSS_SELECTOR, "td:nth-child(3) > img")
+            boj_tier = boj_tier_elem.get_attribute("src").split("/")[-1].replace(".svg", "")
+            save_data.append(boj_data(boj_num, boj_id,boj_problem, boj_tier))
+    
+    print(len(save_data))
+        
 finally:
     driver.quit()
+
