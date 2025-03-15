bd9e197f5d397da8d4566b2108ac105814da609b
김태현 <142918648+Lunamana00@users.noreply.github.com>
2025-03-15 18:43:44 +0900

    [FEAT] Crawling Finish

--- testing.py
+++ testing.py
@@ -3,12 +3,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
+import re
 
 # Chrome 드라이버 경로 설정
 chrome_driver_path = './chromedriver_win64/chromedriver.exe'  
 db_path = './DB'
 db_path_keys = db_path+'/keys.txt'
 db_path_cp = db_path+'/checkpoint.txt'
+
 with open(db_path_keys, "r", encoding="utf-8") as file:
     content = file.read()
 
@@ -18,9 +20,7 @@ with open(db_path_keys, "r", encoding="utf-8") as file:
 with open(db_path_cp, "r", encoding="utf-8") as file:
     content = file.read()
 
-    cp = content
-    print(len(cp))
-    print(cp)
+    check_point = content
 
 chrome_options = Options()
 #chrome_options.add_argument("--headless")  # 브라우저 창을 띄우지 않음
@@ -32,12 +32,12 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 
 try:
-    
-    # 로그인인
+
+    # 로그인
     boj_login = "https://www.acmicpc.net/login?next=%2F"
     driver.get(boj_login)
 
-    time.sleep(3)
+    time.sleep(2)
     
     id_input = driver.find_element(By.CSS_SELECTOR, "#login_form > div:nth-child(2) > input")
     id_input.send_keys(id)
@@ -48,40 +48,76 @@ try:
     login_button = driver.find_element(By.CSS_SELECTOR, "#submit_button")
     login_button.click()
 
-    time.sleep(3)
-    
+    time.sleep(2)
+
     driver.get("https://www.acmicpc.net/status?group_id=23106")
 
     time.sleep(2)
 
     catch_flag = 1
     first_elem = 0
+    elem_read = 123
 
     table_rows = driver.find_elements(By.CSS_SELECTOR, "table.table tbody tr")
 
     class boj_data:
-        def __init__(self,num,id,problem,tier):
+        def __init__(self,num,id,problem,tier,date):
             self.num = num
             self.id = id
             self.problem = problem
             self.tier  = tier
+            self.date = date
+
+    class boj_date:
+        def __init__(self,year,month,day):
+            self.year = year
+            self.month = month
+            self.day = day
+
 
     # 각 행의 데이터를 추출
     save_data = list()
+    while True:
 
-    for row in table_rows:
-        if first_elem == cp:
+        for row in table_rows:
+
+            if row.find_element(By.CLASS_NAME, 'result').text=='맞았습니다!!':
+
+                boj_num = row.find_elements(By.TAG_NAME, 'td')[0].text
+                elem_read = boj_num
+
+                if first_elem == 0:
+                    first_elem = boj_num
+
+                if elem_read == check_point:
+                    break
+
+                boj_id = row.find_elements(By.TAG_NAME, 'td')[1].text
+                boj_problem = row.find_elements(By.TAG_NAME, 'td')[2].text[1:]
+                boj_tier_elem = row.find_element(By.CSS_SELECTOR, "td:nth-child(3) > img")
+                boj_tier = boj_tier_elem.get_attribute("src").split("/")[-1].replace(".svg", "")
+                elem = driver.find_element(By.CSS_SELECTOR, "a.real-time-update.show-date")
+                data_title = elem.get_attribute("data-original-title")
+
+                # regex로 년 월 일 추출
+                match = re.search(r'(\d{4})년\s*(\d{1,2})월\s*(\d{1,2})일', data_title)
+                if match:
+                    year, month, day = match.groups()
+
+                save_data.append(boj_data(boj_num, boj_id,boj_problem, boj_tier,boj_date(year,month,day)))
+
+        if elem_read != check_point:
+            next_button =driver.find_element(By.CSS_SELECTOR, "#next_page")
+            next_button.click()
+
+            time.sleep(2)
+
+            table_rows = driver.find_elements(By.CSS_SELECTOR, "table.table tbody tr")
+        else:
             break
-        if row.find_element(By.CLASS_NAME, 'result').text=='맞았습니다!!':
-            boj_num = row.find_elements(By.TAG_NAME, 'td')[0].text
-            boj_id = row.find_elements(By.TAG_NAME, 'td')[1].text
-            boj_problem = row.find_elements(By.TAG_NAME, 'td')[2].text[1:]
-            boj_tier_elem = row.find_element(By.CSS_SELECTOR, "td:nth-child(3) > img")
-            boj_tier = boj_tier_elem.get_attribute("src").split("/")[-1].replace(".svg", "")
-            save_data.append(boj_data(boj_num, boj_id,boj_problem, boj_tier))
     
-    print(len(save_data))
-        
+    with open(db_path_cp, "w", encoding="utf-8") as file:
+        file.write(first_elem)
+
 finally:
     driver.quit()
-
